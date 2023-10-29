9e08b75f9cab004071cee159b7a620650a343266
Antonkorolev <antonkorolev1994@gmail.com>
2023-10-29 22:19:50 +0300

    add BackendService

---
+++ BackendService/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var configuration = builder.Configuration;
+
+// Add services to the container.
+configuration.AddJsonFile("appsettings.json");
+configuration.AddJsonFile($"appsettings.{env}.json", optional: true);
+configuration.AddEnvironmentVariables();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
