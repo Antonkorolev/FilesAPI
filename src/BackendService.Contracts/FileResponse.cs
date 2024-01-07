dcf2b7744a1b6ff749d1e09d1aadb9ed0da2efb6
Antonkorolev <antonkorolev1994@gmail.com>
2024-01-07 20:33:54 +0300

    change FileResponse

--- src/BackendService.Contracts/FileResponse.cs
+++ src/BackendService.Contracts/FileResponse.cs
@@ -2,5 +2,16 @@ namespace BackendService.Contracts;
 
 public sealed class FileResponse
 {
-    
+    public FileResponse(string name, Stream stream, string contentType)
+    {
+        Name = name;
+        Stream = stream;
+        ContentType = contentType;
+    }
+
+    public string Name { get; set; }
+
+    public Stream Stream { get; set; }
+
+    public string ContentType { get; set; }
 }
